6a1d95a46a2ab5a117dab182e7fcdb233f1bccb0
Prahlad BK <prahladbk50@gmail.com>
2024-09-06 12:54:20 +0530

    s

--- script.js
+++ script.js
@@ -473,5 +473,5 @@ is_user_logged_in()
   .catch((error) => {
     reset_purchase_button();
   });
-  console.log("test--",order_details.purchase_units[0].payments[intent_object][0].status);
+  console.log("test--");
   
